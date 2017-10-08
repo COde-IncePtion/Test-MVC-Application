5a2e53940d4aa4aa24c2eb849fdbb0148461181b
admin <admin@in-rashish.local>
2017-10-08 21:10:32 +0530

    adding a custom controller Album and sending data to view using ViewData Dictonary available

---
+++ TestMvcApplication/Controllers/AlbumController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestMvcApplication.Controllers
+{
+    public class AlbumController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            string message = "Hey there... it's Ashish Rawat";
+            ViewData["message"] = message;
+            
+            return View();
+        }
+    }
+}
--- TestMvcApplication/Program.cs
+++ TestMvcApplication/Program.cs
@@ -20,6 +20,7 @@ namespace TestMvcApplication
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
     }
 }
